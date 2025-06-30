4cb1f4126f5627ee574a99cb15d9fe434b19a01c
perogis <antropovdavid85@gmail.com>
2025-06-30 16:37:18 +0200

    random places fackts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,16 +12,14 @@ const Home = () => {
             const timeout = setTimeout(() => {
                 setDisplayedText((prev) => prev + text.charAt(index));
 
-                // Сперва увеличим индекс
                 setIndex((prevIndex) => {
                     const newIndex = prevIndex + 1;
 
-                    // Проверка после увеличения
-                    if (newIndex === 11) { // потому что index увеличивается *после* добавления символа
+                    if (newIndex === 11) {
                         setShowBorder(false);
                         setTimeout(() => {
                             setShowBorder(true);
-                        }, 300); // на сколько миллисекунд убрать бордер
+                        }, 300);
                     }
 
                     return newIndex;
@@ -48,14 +46,18 @@ const Home = () => {
             </p>
 
             <div className="all-facts">
-                <button onClick={() => toggleQuestion(1)}>время потраченное на создание сайта</button>
-                {activeQuestion === 1 && <div className="faq-answer">на данный момент потраченно около полугода</div>}
-
-                <button onClick={() => toggleQuestion(2)}>история создания сайта</button>
-                {activeQuestion === 2 && <div className="faq-answer">изначально сайт создавался для школьного проекта однако со временем переписался под портфолио</div>}
-
-                <button onClick={() => toggleQuestion(3)}>Создатели сайта</button>
-                {activeQuestion === 3 && <div className="faq-answer">Денис Харитончик, Давид Антропов</div>}
+                <div className='faq-answer_one'>
+                    <button onClick={() => toggleQuestion(1)}>время потраченное на создание сайта</button>
+                    {activeQuestion === 1 && <div className="faq-answer">на данный момент потраченно около полугода</div>}
+                </div>
+                <div className='faq-answer_two'>
+                    <button onClick={() => toggleQuestion(2)}>история создания сайта</button>
+                    {activeQuestion === 2 && <div className="faq-answer">изначально сайт создавался для школьного проекта однако со временем переписался под портфолио</div>}
+                </div>
+                <div className='faq-answer_three'>
+                    <button onClick={() => toggleQuestion(3)}>cколько яблок я сел за жизнь</button>
+                    {activeQuestion === 3 && <div className="faq-answer">я не знаю</div>}
+                </div>
             </div>
         </div>
     );
