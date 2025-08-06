be672e45278e22d43c974ef4a693e20951a8ba9b
DenisHrtn <denispython16@gmail.com>
2025-08-06 19:40:41 +0300

    MS-78: API Client

---
+++ src/api/client.js
@@ -0,0 +1,26 @@
+const API_URL = process.env.REACT_APP_API_URL;
+
+export const request = async (endpoint, options = {}) => {
+  const url = `${API_URL}${endpoint}`;
+
+  const defaultHeaders = {
+    'Content-Type': 'application/json',
+  };
+
+  const config = {
+    ...options,
+    headers: {
+      ...defaultHeaders,
+      ...options.headers,
+    },
+  };
+
+  const response = await fetch(url, config);
+
+  if (!response.ok) {
+    const errorBody = await response.json().catch(() => ({}));
+    throw new Error(errorBody.detail || 'API Error');
+  }
+
+  return await response.json();
+};
